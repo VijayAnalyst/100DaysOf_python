871c6155c2b47f377046c89267c5c937d33a5f13
Vijaykumar <68533393+liferacervijay@users.noreply.github.com>
2024-03-24 13:02:30 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,6 @@
-print("Hello, world!")
+print("Hello!")
+
+My_Name = "Vijay"
+Your_Name = input("Hey stranger, could you please enter your name : ")
+
+print(f"Its pleasure to know your name, my name is {My_Name.upper()} and nice to meet you {Your_Name.upper()}.")
